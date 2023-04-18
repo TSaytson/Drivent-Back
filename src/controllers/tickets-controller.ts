999f7c572f7050b88500125d14dff2704f69c165
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2023-04-18 13:41:03 -0300

    [fix] tickets type route

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
 import ticketsService from '@/services/tickets-service';
+import enrollmentRepository from '@/repositories/enrollment-repository';
 
 export async function getTicketsTypes(req: AuthenticatedRequest, res: Response) {
   const ticketsTypes = await ticketsService.getTicketsTypes();
@@ -10,5 +11,10 @@ export async function getTicketsTypes(req: AuthenticatedRequest, res: Response) 
 }
 
 export async function getTickets(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const enrollments = await enrollmentRepository.findWithAddressByUserId(userId);
+  console.log(enrollments);
   const tickets = await ticketsService.getTickets();
+
+  return res.status(200).send(tickets);
 }
